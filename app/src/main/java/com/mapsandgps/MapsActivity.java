9ecb5468ad3b95f654ffb1a2aa757edcc47fc7bc
Dominik277 <dominikdoric277@gmail.com>
2020-11-06 23:02:33 +0100

    Mjenjanje lokacije i mjenjanje izgleda mape.

--- app/src/main/java/com/mapsandgps/MapsActivity.java
+++ app/src/main/java/com/mapsandgps/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -37,10 +38,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng MountEverest = new LatLng(27.988119, 86.9074655);
+        mMap.addMarker(new MarkerOptions().position(MountEverest)
+                .title("Biggest in the world!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MountEverest,7));
     }
 }
