8ba21135cc745f6a847e29aaa73d5c016815e3dd
Dominik277 <dominikdoric277@gmail.com>
2020-11-07 09:17:12 +0100

    Upisivanje longitude i latitude i ispisivanje u konzoli informacije o mjestu.

--- app/src/main/java/com/mapsandgps/MapsActivity.java
+++ app/src/main/java/com/mapsandgps/MapsActivity.java
@@ -8,11 +8,14 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,6 +25,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -77,6 +84,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.addMarker(new MarkerOptions().position(userLocation)
                         .title("Your location!"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAddreses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                    if (listAddreses != null && listAddreses.size() > 0){
+                        String address = "";
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
